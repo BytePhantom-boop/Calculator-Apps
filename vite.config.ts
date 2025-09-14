6fd452078c754a170a9245b377e41440eeafcb78
BytePhantom-boop <f20250443@hyderabad.bits-pilani.ac.in>
2025-09-14 22:02:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "./",   // 👈 ADD THIS LINE
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -38,3 +39,4 @@ export default defineConfig({
     },
   },
 });
+
