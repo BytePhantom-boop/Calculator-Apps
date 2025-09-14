44a75119089f939bf4bbab9cba36b0c6e8959105
BytePhantom-boop <f20250443@hyderabad.bits-pilani.ac.in>
2025-09-14 20:18:51 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.calculator.app',
   appName: 'Calculator',
-  webDir: 'dist'
+  webDir: 'dist/public'
 };
 
 export default config;
