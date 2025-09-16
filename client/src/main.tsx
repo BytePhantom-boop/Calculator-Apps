33e4a8cf98910f88948da78b5de13655fe2151e9
BytePhantom-boop <f20250443@hyderabad.bits-pilani.ac.in>
2025-09-16 19:01:35 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,17 @@
+// Debug fetch calls (add this before any imports)
+const originalFetch = window.fetch;
+window.fetch = async (...args) => {
+  console.log("FETCH CALL:", args);
+  try {
+    const response = await originalFetch(...args);
+    console.log("FETCH RESPONSE:", response);
+    return response;
+  } catch (err) {
+    console.error("FETCH ERROR:", err);
+    throw err;
+  }
+};
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
