96f54d5a4f53d80054771a59692c14f6e321792c
BytePhantom-boop <f20250443@hyderabad.bits-pilani.ac.in>
2025-09-16 19:22:40 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,16 +5,14 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
-
 function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
+ return (
+   <Switch>
+     <Route path="/" component={Home} />
+     <Route component={NotFound} />
+   </Switch> 
+ );
 }
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
